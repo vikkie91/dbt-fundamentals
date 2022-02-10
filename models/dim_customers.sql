429a0181d870c2c0f2b1535896b7fddb284c8d76
vigneshraghavan@gmail.com <vigneshraghavan@gmail.com>
2022-02-10 01:10:01 +0000

    created stg_customer and stg_orders

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -5,19 +5,17 @@
 
 
 with customers as(
-    select * from {{ref('stg_customers')}}
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-    select * from {{ref('stg_orders')}}
+    select * from {{ ref('stg_orders') }}
 ),
-
 
 customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
@@ -45,4 +43,4 @@ final as (
 
 )
 
-select * from final;
+select * from final
